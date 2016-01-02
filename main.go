83be32d9e3c01b6316e06ac55d9082cec84f5e0f
Asim <asim@chuhnk.me>
2016-01-02 20:57:17 +0000

    Use service definition

--- main.go
+++ main.go
@@ -1,57 +1,53 @@
 package main
 
 import (
-	"github.com/codegangsta/cli"
 	log "github.com/golang/glog"
-	"github.com/micro/go-micro/cmd"
-	"github.com/micro/go-micro/server"
+	"github.com/micro/cli"
+	"github.com/micro/go-micro"
 	"github.com/micro/twitter-srv/handler"
 	proto "github.com/micro/twitter-srv/proto/api"
 	"github.com/micro/twitter-srv/twitter"
 )
 
 func main() {
-	cmd.Flags = append(cmd.Flags,
-		cli.StringFlag{
-			Name:   "access_token",
-			EnvVar: "ACCESS_TOKEN",
-			Usage:  "Twitter access token",
-		},
-		cli.StringFlag{
-			Name:   "access_token_secret",
-			EnvVar: "ACCESS_TOKEN_SECRET",
-			Usage:  "Twitter access token secret",
-		},
-		cli.StringFlag{
-			Name:   "consumer_key",
-			EnvVar: "CONSUMER_KEY",
-			Usage:  "Twitter consumer key",
-		},
-		cli.StringFlag{
-			Name:   "consumer_secret",
-			EnvVar: "ACCESS_TOKEN",
-			Usage:  "Twitter consumer secret",
-		},
+	service := micro.NewService(
+		micro.Name("go.micro.srv.twitter"),
+		micro.Flags(
+			cli.StringFlag{
+				Name:   "access_token",
+				EnvVar: "ACCESS_TOKEN",
+				Usage:  "Twitter access token",
+			},
+			cli.StringFlag{
+				Name:   "access_token_secret",
+				EnvVar: "ACCESS_TOKEN_SECRET",
+				Usage:  "Twitter access token secret",
+			},
+			cli.StringFlag{
+				Name:   "consumer_key",
+				EnvVar: "CONSUMER_KEY",
+				Usage:  "Twitter consumer key",
+			},
+			cli.StringFlag{
+				Name:   "consumer_secret",
+				EnvVar: "ACCESS_TOKEN",
+				Usage:  "Twitter consumer secret",
+			},
+		),
+		micro.Action(func(c *cli.Context) {
+			twitter.Token = c.String("access_token")
+			twitter.TokenSecret = c.String("access_token_secret")
+			twitter.ConsumerKey = c.String("consumer_key")
+			twitter.ConsumerSecret = c.String("consumer_secret")
+		}),
 	)
 
-	cmd.Actions = append(cmd.Actions, func(c *cli.Context) {
-		twitter.Token = c.String("access_token")
-		twitter.TokenSecret = c.String("access_token_secret")
-		twitter.ConsumerKey = c.String("consumer_key")
-		twitter.ConsumerSecret = c.String("consumer_secret")
-	})
-
-	cmd.Init()
-
+	service.Init()
 	twitter.Init()
 
-	server.Init(
-		server.Name("go.micro.srv.twitter"),
-	)
+	proto.RegisterApiHandler(service.Server(), &handler.Api{})
 
-	proto.RegisterApiHandler(server.DefaultServer, &handler.Api{})
-
-	if err := server.Run(); err != nil {
+	if err := service.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
